e6312077eaa2388e71a2f825cce69c9f055abc3f
timothy1202 <timothy1202@naver.com>
2023-11-26 19:39:28 +0900

    아이템 생성

---
+++ NetworkProject_CrazyArcade/Assets/Scripts/Content/Box/CreateRandomItem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRandomItem : MonoBehaviour
+{
+    public GameObject[] objectsToSpawn; // 생성할 오브젝트 배열
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "BombStream")
+        {
+            Destroy(gameObject);
+            SpawnRandomObject();
+        }
+    }
+
+    void SpawnRandomObject()
+    {
+        int randomIndex = Random.Range(0, objectsToSpawn.Length); // 랜덤 인덱스 생성
+        Instantiate(objectsToSpawn[randomIndex], transform.position, Quaternion.identity); // 랜덤 오브젝트 생성
+    }
+}
---
+++ NetworkProject_CrazyArcade/Assets/Scripts/Content/Player/PlayerStat.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{
+    //[SerializeField]
+    private float playerSpeed;
+    private int numberOfBombs;
+    private int bombLength;
+}
