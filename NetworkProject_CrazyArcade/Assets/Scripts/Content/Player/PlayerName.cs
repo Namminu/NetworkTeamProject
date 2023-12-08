e5cdc858d40011c63600cb7628ea682d12f036a2
20191143namminwoo <mvpalsdn771@gmail.com>
2023-12-08 18:23:47 +0900

    LobbyLevel Cavas 수정

--- NetworkProject_CrazyArcade/Assets/Scripts/Content/Player/PlayerName.cs
+++ NetworkProject_CrazyArcade/Assets/Scripts/Content/Player/PlayerName.cs
@@ -20,7 +20,7 @@ public class PlayerName : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
+     
     private void Update()
     {
         if(Input.GetMouseButtonDown(0))
@@ -33,22 +33,26 @@ public class PlayerName : MonoBehaviour
     //CreateNewRoom UI 띄우기
     public void OnCilickCreateUIONButton()
     {
+        Debug.Log("방 만들기 버튼 입력 : 창 띄우기");
         gameObject.SetActive(true);
     }
 	//CreateNewRoom UI 지우기
 	public void OnCilickCreateUIOFFButton()
 	{
+		Debug.Log("방 만들기 버튼 입력 : 창 내리기");
 		gameObject.SetActive(false);
 	}
 
 	//Password UI 띄우기 
 	public void OnCilickPasswordUIONButton()
     {
+		Debug.Log("방 비밀번호 입력 버튼 입력 : 창 띄우기");
 		gameObject.SetActive(true);
 	}
 	//Password UI 지우기
 	public void OnCilickPasswordUIOFFButton()
 	{
+		Debug.Log("방 비밀번호 입력 버튼 입력 : 창 내리기");
 		gameObject.SetActive(false);
 	}
 	/*----------- Wating Scene ------------- */
