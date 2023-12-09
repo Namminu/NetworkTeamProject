d5bc42309956478635ed41251a5a449bfe4a8feb
timothy1202 <timothy1202@naver.com>
2023-12-09 22:09:12 +0900

    로비 UI 구현 (완료)
    비번방 입장 제외
    방 삭제코드는 임시 코드

--- NetworkProject_CrazyArcade/Assets/Scripts/Content/Player/PlayerName.cs
+++ NetworkProject_CrazyArcade/Assets/Scripts/Content/Player/PlayerName.cs
@@ -28,32 +28,4 @@ public class PlayerName : MonoBehaviour
             Debug.Log(playerName);
         }
     }
-
-    /*----------- Lobby Scene ----------------*/
-    //CreateNewRoom UI 띄우기
-    public void OnCilickCreateUIONButton()
-    {
-        Debug.Log("방 만들기 버튼 입력 : 창 띄우기");
-        gameObject.SetActive(true);
-    }
-	//CreateNewRoom UI 지우기
-	public void OnCilickCreateUIOFFButton()
-	{
-		Debug.Log("방 만들기 버튼 입력 : 창 내리기");
-		gameObject.SetActive(false);
-	}
-
-	//Password UI 띄우기 
-	public void OnCilickPasswordUIONButton()
-    {
-		Debug.Log("방 비밀번호 입력 버튼 입력 : 창 띄우기");
-		gameObject.SetActive(true);
-	}
-	//Password UI 지우기
-	public void OnCilickPasswordUIOFFButton()
-	{
-		Debug.Log("방 비밀번호 입력 버튼 입력 : 창 내리기");
-		gameObject.SetActive(false);
-	}
-	/*----------- Wating Scene ------------- */
 }
