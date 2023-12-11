38616cdcae01c8f90b0aa6f6b435924fc8433ae7
rootinge <jhpark5129@gmail.com>
2023-12-12 02:52:25 +0900

    수정
    수정

--- NetworkProject_CrazyArcade/Assets/Scripts/Content/Item/BaseItem.cs
+++ NetworkProject_CrazyArcade/Assets/Scripts/Content/Item/BaseItem.cs
@@ -13,7 +13,7 @@ public abstract class BaseItem : MonoBehaviourPun
     {
         pv = gameObject.GetComponent<PhotonView>();
     }
-    void OnTriggerEnter2D(Collider2D other) 
+    void OnTriggerStay2D(Collider2D other) 
     {
         if (other.gameObject.tag == "Player")
         {
@@ -29,7 +29,7 @@ public abstract class BaseItem : MonoBehaviourPun
                 }
             }
         }
-
+        
         if(other.gameObject.tag == "BombStream")
         {
             Destroy(gameObject);         
@@ -41,4 +41,5 @@ public abstract class BaseItem : MonoBehaviourPun
     {
         Destroy(gameObject); 
     }
+
 }
