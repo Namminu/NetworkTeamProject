563e53ddbafa09f3482a33561f26afc2c89b6eff
20191143namminwoo <mvpalsdn771@gmail.com>
2023-12-11 23:25:56 +0900

    Arrow UI 수정

--- NetworkProject_CrazyArcade/Assets/Scripts/Content/Player/ShowWhereAmI.cs
+++ NetworkProject_CrazyArcade/Assets/Scripts/Content/Player/ShowWhereAmI.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
 
-public class ShowWhereAmI : MonoBehaviour
+public class ShowWhereAmI : MonoBehaviourPun
 {
     public Canvas Arrow;
-
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("deleteImage", 2.5f);
+        if(photonView.IsMine)
+        {
+            Arrow.gameObject.SetActive(true);
+			Invoke("deleteImage", 2.5f);
+		}        
     }
     private void deleteImage()
     {
